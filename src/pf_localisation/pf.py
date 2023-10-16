4cb966639538cb19b13351c595f109aa8ecb7d85
Manni <cmlmanni@gmail.com>
2023-10-16 21:30:35 +0100

    TODO: fixing algo in update_particle_cloud(self, scan)
    UPDATED:
    removed all z-values
    fixed all for loops
    fixed distance formula

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -19,15 +19,15 @@ class PFLocaliser(PFLocaliserBase):
         # ----- Call the superclass constructor
         super(PFLocaliser, self).__init__()
 
-        self.NUMBER_OF_PARTICLES = 50
+        self.NUMBER_OF_PARTICLES = 50 
         self.INIT_VARIANCE = 2
-        self.EPSILON = 5
+        self.EPSILON = 2
         self.NEIGHBOUR_LIM = 3
 
         # ----- Set motion model parameters CHANGE THESE??
-        self.ODOM_ROTATION_NOISE = 0.5  # Odometry model rotation noise
-        self.ODOM_TRANSLATION_NOISE = 0.5  # Odometry x-axis (forward) noise
-        self.ODOM_DRIFT_NOISE = 0.5  # Odometry y axis (side-side) noise
+        self.ODOM_ROTATION_NOISE = 0.9  # Odometry model rotation noise
+        self.ODOM_TRANSLATION_NOISE = 0.9  # Odometry x-axis (forward) noise
+        self.ODOM_DRIFT_NOISE = 0.9  # Odometry y axis (side-side) noise
 
         # ----- Sensor model parameters
         self.NUMBER_PREDICTED_READINGS = 20  # Number of readings to predict
@@ -49,14 +49,13 @@ class PFLocaliser(PFLocaliserBase):
 
         particles = []
 
-        h = std_msgs.msg.Header()
-        h.stamp = rospy.Time.now()
+        # h = std_msgs.msg.Header()
+        # h.stamp = rospy.Time.now()
 
         for i in range(self.NUMBER_OF_PARTICLES):
             particle = Pose()
-            particle.position.x = initialpose.pose.pose.position.x + (gauss(initialpose.pose.pose.position.x, self.INIT_VARIANCE) * self.ODOM_TRANSLATION_NOISE)
-            particle.position.y = initialpose.pose.pose.position.y + (gauss(initialpose.pose.pose.position.y, self.INIT_VARIANCE) * self.ODOM_DRIFT_NOISE)
-            particle.position.z = initialpose.pose.pose.position.z  # We don't think we need to add noise to the z axis ???
+            particle.position.x = initialpose.pose.pose.position.x + (normalvariate(0, self.INIT_VARIANCE) * self.ODOM_TRANSLATION_NOISE)
+            particle.position.y = initialpose.pose.pose.position.y + (normalvariate(0, self.INIT_VARIANCE) * self.ODOM_DRIFT_NOISE)
 
             heading = getHeading(initialpose.pose.pose.orientation)
             random_heading = gauss(heading, self.INIT_VARIANCE)
@@ -66,7 +65,8 @@ class PFLocaliser(PFLocaliserBase):
 
             particles.append(particle)
 
-        particle_cloud = PoseArray(h, particles)
+        particle_cloud = PoseArray()
+        particle_cloud.poses = particles
 
         return particle_cloud
 
@@ -80,37 +80,40 @@ class PFLocaliser(PFLocaliserBase):
 
          """
         # Loop through the cloud, for each pose get_weight
+
         weights = []
 
-        h = std_msgs.msg.Header()
-        h.stamp = rospy.Time.now()
+        # h = std_msgs.msg.Header()
+        # h.stamp = rospy.Time.now()
 
         for pose in self.particlecloud.poses:
-            weights += self.sensor_model.get_weight(scan, pose)
+            weights.append(self.sensor_model.get_weight(scan, pose))
 
         # Do roulette to determine how many dupes of the pose
         new_particles = []
 
         thresholds = [weights[0]]
+        m = len(weights)
 
-        for i in range(1, len(weights) - 1):
-            thresholds += thresholds[i - 1] + weights[i]
+        for i in range(1, m):
+            thresholds.append(thresholds[i - 1] + weights[i])
 
-        u = random.uniform(0, 1 / len(weights))
+        u = uniform(0, 1/m)
 
         # instantiate new poses
         i = 0
 
-        for j in range(len(weights) - 1):
+        for j in range(m):
+            # print('[Start of j] Amount of particles: ' + str(len(self.particlecloud.poses)))
+
             while u > thresholds[i]:
                 i += 1
 
-            old_p = self.particlecloud.poses[j]
+            old_p = self.particlecloud.poses[i]
 
             new_p = Pose()
-            new_p.position.x = old_p.pose.pose.position.x + (gauss(old_p.pose.pose.position.x, self.INIT_VARIANCE) * self.ODOM_TRANSLATION_NOISE)
-            new_p.position.y = old_p.pose.pose.position.y + (gauss(old_p.pose.pose.position.y, self.INIT_VARIANCE) * self.ODOM_DRIFT_NOISE)
-            new_p.position.z = old_p.pose.pose.position.z  # We don't think we need to add noise to the z axis ???
+            new_p.position.x = old_p.position.x + (normalvariate(0, self.INIT_VARIANCE) * self.ODOM_TRANSLATION_NOISE)
+            new_p.position.y = old_p.position.y + (normalvariate(0, self.INIT_VARIANCE) * self.ODOM_DRIFT_NOISE)  # We don't think we need to add noise to the z axis ???
 
             heading = getHeading(old_p.orientation)
             random_heading = gauss(heading, self.INIT_VARIANCE)
@@ -121,10 +124,10 @@ class PFLocaliser(PFLocaliserBase):
             # add to new posearray
             new_particles.append(new_p)
 
-            u = u + 1 / len(weights)
+            u = u + 1 / m
 
         # update self.particlecloud
-        self.particlecloud = new_particles
+        self.particlecloud.poses = new_particles
 
     def estimate_pose(self):
         """
@@ -151,27 +154,27 @@ class PFLocaliser(PFLocaliserBase):
         for i in range(self.NUMBER_OF_PARTICLES):
             neighbours_i = []
             for j in range(i, self.NUMBER_OF_PARTICLES):
-                p_i = self.particlecloud[i]
-                p_j = self.particlecloud[j]
+                if i != j:
+                    p_i = self.particlecloud.poses[i]
+                    p_j = self.particlecloud.poses[j]
 
-                dist = (p_i.pose.position.x - p_j.pose.position.x) ** 2 + (p_i.pose.position.y - p_j.pose.position.y) ** 2 + (
-                            p_i.pose.position.z - p_j.pose.position.z) ** 2
+                    dist = math.sqrt((p_i.position.x - p_j.position.x) ** 2 + (p_i.position.y - p_j.position.y) ** 2)
 
-                if dist <= self.EPSILON:  # If distance to other pose is less than our set max then we have a neighbour
-                    neighbours_i += [p_j]
+                    if dist <= self.EPSILON:  # If distance to other pose is less than our set max then we have a neighbour
+                        neighbours_i.append(p_j)
             neighbours.append(neighbours_i)
 
-        # Now we have to cluster the neighbours
-        for k in range(self.NUMBER_OF_PARTICLES):
-            for m in range(len(neighbours[k]) - 1):
-                neighbours[k] += neighbours[m]
+        # # Now we have to cluster the neighbours
+        # for k in range(self.NUMBER_OF_PARTICLES):
+        #     for m in range(len(neighbours[k]) - 1):
+        #         neighbours[k] += neighbours[m]
 
         # Now we find the largest cluster
 
         top_len = -1
         top_index = -1
 
-        for n in range(len(neighbours) - 1):
+        for n in range(len(neighbours)):
             curr_len = len(neighbours[n])
             if curr_len > top_len:
                 top_index = n
@@ -179,23 +182,21 @@ class PFLocaliser(PFLocaliserBase):
 
         # Now we take the average of the cluster
 
-        total_xyzh = [0, 0, 0, 0]
+        total_xyzh = [0, 0, 0]
 
-        for i in range(top_len - 1):
+        for i in range(top_len):
             current = neighbours[top_index][i] #may need pose pose
             total_xyzh[0] += current.position.x
             total_xyzh[1] += current.position.y
-            total_xyzh[2] += current.position.z
-            total_xyzh[3] += getHeading(current.orientation)
-
+            total_xyzh[2] += getHeading(current.orientation)
+        
         pose = Pose()
         pose.position.x = total_xyzh[0] / top_len
         pose.position.y = total_xyzh[1] / top_len
-        pose.position.z = total_xyzh[2] / top_len
 
-        avg_heading = total_xyzh[3] / top_len
+        avg_heading = total_xyzh[2] / top_len
         change = getHeading(neighbours[top_index][0].orientation) - avg_heading
 
-        pose.orientation = rotateQuaternion(neighbours[top_index][0], change)
+        pose.orientation = rotateQuaternion(neighbours[top_index][0].orientation, change)
 
         return pose
